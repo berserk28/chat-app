6ba3722d52006d0992ad2d5a0e22fbf33b343ff6
saker toufik <absolutefreeman@gmail.com>
2023-03-13 17:59:28 +0100

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8VRrv0LqyYsjByfIKRxeXUlxhXAX_Pig",
+  authDomain: "chat-94c74.firebaseapp.com",
+  projectId: "chat-94c74",
+  storageBucket: "chat-94c74.appspot.com",
+  messagingSenderId: "1058416546551",
+  appId: "1:1058416546551:web:d3cc895f8298f5442ab9f2",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
