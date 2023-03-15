125bfa841348491eed9514b792328ca97ec26757
saker toufik <absolutefreeman@gmail.com>
2023-03-15 21:53:05 +0100

    Added firebase

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import avatar from "../images/avatar.jpg";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 const Register = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const avatar = e.target[3].value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">fast chat</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={submitHandler}>
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
