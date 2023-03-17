f1ec44f831dd6a6f53affddca1941dda11e33290
saker toufik <absolutefreeman@gmail.com>
2023-03-17 18:14:22 +0100

    styli

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,27 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import avatar from "../images/avatar.jpg";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 const Register = () => {
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     const name = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const avatar = e.target[3].value;
-
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        // ...
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
+    console.log(avatar);
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log("wrong");
+    }
   };
 
   return (
