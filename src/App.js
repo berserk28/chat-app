1dc184a0a078e3677759a2a785d33e4f929fb91b
saker toufik <absolutefreeman@gmail.com>
2023-04-05 11:48:43 +0100

    Updated user profile

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import { useContext } from "react";
 import "./style.scss";
 function App() {
   const { currentUser } = useContext(AuthContext);
-
+  const ProctectedRoute = ({ children }) => {
+    if (!currentUser) return <Navigate to="/login" />;
+    return children;
+  };
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route>
-            <Route index element={currentUser ? <Home /> : <Register />} />
+            <Route
+              index
+              element={
+                <ProctectedRoute>
+                  <Home />
+                </ProctectedRoute>
+              }
+            />
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
           </Route>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 const Login = () => {
+  const [error, setError] = useState(false);
+  const [setLoading, loading] = useState(false);
+  const navigate = useNavigate();
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    console.log(email, password);
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      console.log(res);
+      navigate("/");
+      setError(true);
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">fast chat</span>
         <span className="title">Login</span>
-        <form>
+        <form onSubmit={submitHandler}>
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
 
@@ -15,6 +35,7 @@ const Login = () => {
           <p>
             You don't have an account? <Link to={"/register"}>Register</Link>
           </p>
+          {error && <p>something went wrong</p>}
         </form>
       </div>
     </div>
