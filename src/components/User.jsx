88d54d3c59793a93167f24a103a412057b387446
saker toufik <absolutefreeman@gmail.com>
2023-04-20 13:35:46 +0100

    Updated user

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 import { db } from "../firebase";
 const User = ({ user }) => {
   const { setSelectedUser, currentUser } = useContext(AuthContext);
@@ -15,7 +21,27 @@ const User = ({ user }) => {
       const res = await getDoc(doc(db, "chats", combinedID));
       //  if chats collection does not exist create one
       if (!res.exists()) {
-        await setDoc(doc(db, "chats", combinedID));
+        // creating chat collection
+        await setDoc(doc(db, "chats", combinedID), { message: {} });
+
+        // create user
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedID + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedID + ".date"]: serverTimestamp(),
+        });
+        // current user
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedID + ".currentUserInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedID + ".date"]: serverTimestamp(),
+        });
       }
     } catch (error) {
       console.log("chats ERROR");
