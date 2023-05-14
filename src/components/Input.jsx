0b76902439ee666e702259467aecea75fed5cb42
saker toufik <absolutefreeman@gmail.com>
2023-05-14 23:35:41 +0100

    Updatted input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import Img from "../images/img.png";
+import { doc } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 const Input = () => {
+  const { currentUser, selectedUser } = useContext(AuthContext);
+  const messageHandler = (e) => {
+    console.log(e.value);
+  };
+
+  const handleSend = () => {
+    console.log(selectedUser.displayName);
+  };
   return (
     <div className="input">
-      <input type="text" placeholder="Message ..." />
+      <input
+        type="text"
+        placeholder="Message ..."
+        onChange={(e) => console.log(e.target.value)}
+      />
       <div className="send">
         <input type="file" id="img" style={{ display: "none" }} />
         <label htmlFor="img">
           <img src={Img} alt="" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   );
