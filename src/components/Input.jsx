8e6c020347e45cdc7665f29fd6f468eff678258a
saker toufik <absolutefreeman@gmail.com>
2023-05-16 22:47:49 +0100

    Updated input state

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,32 +1,53 @@
 import React, { useContext, useState } from "react";
 import Img from "../images/img.png";
 import { doc, updateDoc, arrayUnion, Timestamp } from "firebase/firestore";
-import { db } from "../firebase";
+import { db, storage } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 import { v4 as uuid } from "uuid";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 const Input = () => {
   const { currentUser, selectedUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
   const [message, setMessage] = useState("");
   const [image, setImage] = useState(null);
-  console.log(image);
   const messageHandler = (e) => {
     console.log(e.value);
   };
 
   const handleSend = async () => {
-    try {
-      await updateDoc(doc(db, "chats", data.chatId), {
-        message: arrayUnion({
-          id: uuid(),
-          text: message,
-          time: Timestamp.now(),
-        }),
+    if (image) {
+      const imageRef = ref(storage, uuid());
+      await uploadBytesResumable(imageRef, image).then(() => {
+        getDownloadURL(imageRef).then(async (downloadURL) => {
+          try {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              message: arrayUnion({
+                id: uuid(),
+                image: downloadURL,
+                time: Timestamp.now(),
+              }),
+            });
+          } catch (error) {
+            console.log("Error in sending the message");
+          }
+        });
       });
-    } catch (error) {
-      console.log("Error in sending the message");
+    } else {
+      try {
+        await updateDoc(doc(db, "chats", data.chatId), {
+          message: arrayUnion({
+            id: uuid(),
+            text: message,
+            time: Timestamp.now(),
+          }),
+        });
+      } catch (error) {
+        console.log("Error in sending the message");
+      }
     }
+    setMessage("");
+    setImage(null);
   };
   return (
     <div className="input">
@@ -40,7 +61,7 @@ const Input = () => {
           type="file"
           id="img"
           style={{ display: "none" }}
-          onChange={(e) => setImage(e.target.value)}
+          onChange={(e) => setImage(e.target.files[0])}
         />
         <label htmlFor="img">
           <img src={Img} alt="" />
